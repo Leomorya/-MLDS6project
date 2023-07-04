a7b458409bb5bb520c47b7c86e4c31a3e99f972d
Leomorya <leomorya@gmail.com>
2023-07-03 19:05:42 -0500

    mejora de las descarga de los datos

--- scripts/data_acquisition/descarga_data.py
+++ scripts/data_acquisition/descarga_data.py
@@ -1,4 +1,3 @@
-
 import time
 import re
 import pandas as pd
@@ -8,6 +7,13 @@ from selenium.webdriver.common.by import By
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome("chromedriver", options=chrome_options)
+import os
+#ruta de la carpeta descargas
+carpeta_descargas = os.path.expanduser("~/Descargas")
+
+#crea la carpeta data
+carpeta_data = os.path.join(carpeta_descargas, "data")
+os.makedirs(carpeta_data, exist_ok=True)
 
 # ruta del archivo a descargar
 ruta_train="https://www.mediafire.com/file/il2ma0ez6wnr1ag/train.csv/file"
@@ -20,10 +26,12 @@ botom=driver.find_element(By.ID,"downloadButton")
 botom.click()
 
 # ruta local
-archivo="./Descargas/train.csv" 
+archivo="./Descargas/data/train.csv" 
 
 # espera a descarga de los datos
 time.sleep(10)
+
+
 
 #carga en un dataframe
 
@@ -41,10 +49,26 @@ botom2 = driver.find_element(By.ID,"downloadButton")
 botom2.click()
 
 # ruta local
-archivo2="./Descargas/test.csv" 
+#archivo2="./Descargas/data/test.csv" 
 
 # espera a descarga de los datos
 time.sleep(10)
 
 #df_test=pd.read_csv(archivo2)
 #display(df_produccion.head(5))
+
+# cambia los archivos descargados a la carpeta data 
+
+ruta_archivo_descargado = os.path.join(carpeta_descargas, "train.csv") # Ruta del archivo descargado
+ruta_destino = os.path.join(carpeta_data, "train.csv")  # Ruta de destino en la carpeta "data"
+
+os.rename(ruta_archivo_descargado, ruta_destino) # guarda el archivo train.csv en la carpeta data
+
+ruta_archivo_descargado = os.path.join(carpeta_descargas, "produccion.csv")  
+ruta_destino = os.path.join(carpeta_data, "produccion.csv")  
+
+os.rename(ruta_archivo_descargado, ruta_destino) # guarda el archivo produccion.csv en la carpeta data
+
+# Cerrar el navegador
+driver.quit()
+
